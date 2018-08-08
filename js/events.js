18bfb6f355e487350df0121c7204e99cbadaa078
Indu Ramesh <induramesh@gmail.com>
2018-08-08 19:54:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,6 @@
 function getIt() {
-  
+  $('p').on("")
+  // The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".
 }
 
 $(document).ready(function(){
