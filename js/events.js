37922a2da0384fc4540da43d40144e4971cde648
Indu Ramesh <induramesh@gmail.com>
2018-08-08 20:08:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('img').on("load", function(){
+  $('form').on("load", function(){
     $("img").addClass("tasty");
 });
 }
@@ -20,5 +20,6 @@ $(document).ready(function(){
 
 getIt();
 frameIt();
+pressIt;
 
 });
