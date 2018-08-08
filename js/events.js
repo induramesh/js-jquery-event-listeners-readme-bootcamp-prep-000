a230da3a4b0396994f51e80b8d82db1189145ec9
Indu Ramesh <induramesh@gmail.com>
2018-08-08 20:14:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,10 +21,12 @@ function pressIt() {
 function submitIt() {
   $("form").on("submit", function(){
     if ($( "input:first" ).val() === "correct") {
-      alert('your form is going to be submitted now');
-    return; }
-  alert("you entered the wrong value");
-  return;
+      window.alert('your form is going to be submitted now');
+    return; 
+      
+    }
+  window.alert("you entered the wrong value");
+    return;
 });
 }
 
