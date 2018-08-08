187f11ccd26018d9fa79c29c11677646bcfa8700
Indu Ramesh <induramesh@gmail.com>
2018-08-08 20:15:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function pressIt() {
 
 function submitIt() {
   $("form").on("submit", function(){
-      window.alert("your form is going to be submitted now");
+      window.alert("your form is going to be submitted now.");
           return; 
 });
 }
